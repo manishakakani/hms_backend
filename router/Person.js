ac60a4aa2d9ebaf1ab047ca8d5316b8f5b133bf8
Poonam Mittal <poonam.mittal@sigtech.co.uk>
2022-11-21 13:27:23 +0530

    person apis

--- router/Person.js
+++ router/Person.js
@@ -16,4 +16,71 @@ router.get("/person", (req, res) => {
     });
 });
 
+router.get("/person/:uniquenumber", (req, res) => {
+  Person.find({ UniqueNumber: req.params.uniquenumber })
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving tutorials.",
+      });
+    });
+});
+
+router.post("/person", async (req, res) => {
+  const data = req.body;
+  const person = new Person(data);
+  try {
+    person
+      .save(person)
+      .then((response) => {
+        res.status(200).send("Successfully inserted!");
+      })
+      .catch((err) => {
+        console.log({ err });
+        res.status(400).send(err);
+      });
+  } catch (error) {
+    console.log({ error });
+    res.status(400).send(error);
+  }
+});
+
+router.patch("/person/:id", async (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({
+      message: "Data to update can not be empty!",
+    });
+  }
+
+  const id = req.params.id;
+
+  Person.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot update person with id=${id}. Maybe person was not found!`,
+        });
+      } else res.send({ message: "Person updated successfully." });
+    })
+    .catch((err) => {
+      console.log({ err });
+      res.status(500).send({
+        message: "Error updating person with id=" + id,
+      });
+    });
+});
+
+router.delete("/person/:id", (req, res) => {
+  try {
+    Person.deleteOne({ id: req.params.id })
+      .then((response) => res.status(200).send("Successfully Deleted!"))
+      .catch((err) => res.status(400).send(err));
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
 module.exports = router;
